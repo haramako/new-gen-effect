2071136d2b51a2be00690f81de238b4cd7f99ebb
HARADA Makoto <harada@toydea.com>
2018-06-15 20:48:03 +0900

    First commit

---
+++ Assets/MotionManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionParam 
+{
+	public GameObject[] Targets;
+}
+
+[ExecuteInEditMode]
+public class MotionManager : MonoBehaviour {
+
+	public GameObject MainChar;
+	public GameObject BlendTarget1;
+	public GameObject BlendTarget2;
+	public float Blend;
+
+	Animator anim_;
+
+	public void PlayMotion(MotionParam p)
+	{
+		anim_ = MainChar.GetComponent<Animator>();
+
+		anim_.SetTrigger("Attack");
+	}
+
+	void LateUpdate()
+	{
+		Vector3 pos = BlendTarget1.transform.position;
+		pos = Vector3.Lerp(pos, BlendTarget2.transform.position, Blend);
+
+		MainChar.transform.position = pos;
+	}
+}
---
+++ Assets/TestScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScene : MonoBehaviour {
+
+	public MotionManager MotionManager;
+
+	public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+			MotionManager.PlayMotion(new MotionParam());
+        }
+    }
+
+}
